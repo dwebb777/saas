959092a143bda57c7e7673ada6e5d906265f5186
David Webb <webbd5050@gmail.com>
2023-09-04 20:40:16 -0400

    fixed mobile nav menu

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ document.querySelector(".menu-toggle").addEventListener("click", function () {
   var menu = document.querySelector(".menu")
   var hero = document.querySelector(".hero")
   var top = document.querySelector(".top")
-  var headbutton = document.querySelector(".header-btn")
+  var headButton = document.querySelector(".header-btn")
+  var menuToggle = document.querySelector(".menu-toggle")
 
   if (menu.style.display === "none") {
     menu.style.display = "flex"
   } else {
     hero.style.display = "none"
     menu.style.display = "none"
+
+    menuToggle.style.marginTop = "14rem"
     top.style.background = "unset"
-    headbutton.style.display = "block"
+    headButton.style.display = "block"
   }
 })
